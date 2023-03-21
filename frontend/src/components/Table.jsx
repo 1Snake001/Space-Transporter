62c62681aede05034192f9e5812abc2138d22317
SnakeMacbookPro <snakemacbookpro@SnakeMacbookPro-iMac-Pro.local>
2023-03-21 13:09:59 +0100

    the tbody component added to the table component

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Thead from "./Thead";
+import Tbody from "./Tbody";
+
 const Table = () => {
   return (
     <table className="table table-bordered">
       <Thead />
+      <Tbody />
     </table>
   );
 };
---
+++ frontend/src/components/Tbody.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tbody = () => {
+  return (
+    <tbody>Tbody</tbody>
+  )
+}
+
+export default Tbody
