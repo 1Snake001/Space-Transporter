a91d761c1527649d86b0b851804363ded12c21df
SnakeMacbookPro <snakemacbookpro@SnakeMacbookPro-iMac-Pro.local>
2023-03-21 15:59:49 +0100

    move here button onclick for spaceship moving was implemented

--- frontend/src/components/TableRow.jsx
+++ frontend/src/components/TableRow.jsx
@@ -1,72 +1,72 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import servicesInstance from "../data-services/dataServices";
 
 const TableRow = ({ isHeader, theadInnerHtml }) => {
-
-    const [planetsData, setPlanetsData] = useState([]);
     const [shipData, setShipData] = useState({});
+    const [planetsData, setPlanetsData] = useState([]);
+    const [planetId, setPlanetId] = useState(0);
 
-useEffect(()=>{
-       const getData = async () => {
-        const planetData = await servicesInstance.getPlanet();
-        const shipData = await servicesInstance.getShip();
-        setPlanetsData(planetData);
-        setShipData(shipData);
-       }
-       getData();
-},[])
+  useEffect(() => {
+    const getData = async () => {
+      const planetData = await servicesInstance.getPlanet();
+      const shipData = await servicesInstance.getShip();
+      setPlanetsData(planetData);
+      setShipData(shipData);
+    };
+    getData();
+  }, [planetId]);
 
+const moveShipHandler = async (planetId) => {
+const updateShipId = await servicesInstance.moveShip(planetId);
+setPlanetId(planetId);
+};
 
+console.log(planetId);
+  if (isHeader) {
+    return (
+      <tr>
+        {Object.values(theadInnerHtml).map((content) => (
+          <td key={content}>{content}</td>
+        ))}
+      </tr>
+    );
+  } else {
+    return (
+      <>
+        {planetsData &&
+          planetsData.map((planet) => (
+            <tr
+              className={
+                shipData.planetId === planet._id ? `table table-dark` : ``
+              }
+              key={planet.name}
+            >
+              <td>{planet.name}</td>
+              <td>{planet.population}</td>
+              <td>
+                {shipData.planetId === planet._id ? (
+                  <>
+                    <button type="button" className="btn btn-success btn-sm">
+                      « toPlanet
+                    </button>
+                    <button type="button" className="btn btn-success btn-sm">
+                      toShip »
+                    </button>{" "}
+                  </>
+                ) : (
+                  <button type="button" onClick={() => moveShipHandler(planet._id)} className="btn btn-primary btn-sm">
+                    Move here
+                  </button>
+                )}
+              </td>
+              <td>
+                {shipData.planetId === planet._id ? shipData.passengers : " - "}
+              </td>
+            </tr>
+          ))}
+      </>
+    );
+  }
+};
 
-    if (isHeader) {
-      return (
-        <tr>
-         {Object.values(theadInnerHtml).map(content => (<td key={content}>{content}</td>))}
-        </tr>
-      );
-    } else {
-      return (
-        <>
-      {planetsData && planetsData.map((planet) => (
-        <tr
-          className={ shipData.planetId === planet._id ? `table table-dark` : ``}
-          key={planet.name}
-        >
-          <td>{planet.name}</td>
-          <td>{planet.population}</td>
-          <td>
-          {shipData.planetId === planet._id ? (
-              <>
-                <button
-                  type="button"
-                  className="btn btn-success btn-sm"
-                >
-                  « toPlanet
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-success btn-sm"
-                >
-                  toShip »
-                </button>{" "}
-              </>
-            ) : (
-              <button
-                type="button"
-                className="btn btn-primary btn-sm"
-              >
-                Move here
-              </button>
-            )}
-          </td>
-          <td>
-            {shipData.planetId === planet._id ? shipData.passengers : " - "}
-          </td>
-        </tr>
-      ))}
-    </>
-      );
-    }
-  };
-
-  export default TableRow;
+export default TableRow;
--- frontend/src/data-services/dataServices.js
+++ frontend/src/data-services/dataServices.js
@@ -1,38 +1,57 @@
 class Services {
-    constructor(url, spaceShipEndpoint, planetEndpoint) {
-      this.url = `http://localhost:5000`;
-      this.planetEndpoint = `/api/planets`;
-      this.spaceShipEndpoint = `/api/spaceship`;
-      this.apiPassengersToShipEndpoint = `/api/passengers/toship`;
-      this.apiPassengersToPlanetpEndpoint = `/api/passengers/toplanet`;
-    }
-  
-    async getPlanet() {
-        try {
-          const response = await fetch(`${this.url}${this.planetEndpoint}`);
-          const data = await response.json();
-      
-          return data;
-        } catch (error) {
-          console.error(`Error ${error}`)
-          throw error;
-        }
-      }
-
-      async getShip() {
-        try {
-          const response = await fetch(`${this.url}${this.spaceShipEndpoint}`);
-          const data = await response.json();
-      
-          return data;
-          
-        } catch (error) {
-          console.error(`Error ${error}`)
-          throw error;
-        }
-      }
-    
+  constructor(url, spaceShipEndpoint, planetEndpoint) {
+    this.url = `http://localhost:5000`;
+    this.planetEndpoint = `/api/planets`;
+    this.spaceShipEndpoint = `/api/spaceship`;
+    this.apiPassengersToShipEndpoint = `/api/passengers/toship`;
+    this.apiPassengersToPlanetpEndpoint = `/api/passengers/toplanet`;
   }
-  
-  const servicesInstance = new Services();
-  export default servicesInstance;
+
+  async getPlanet() {
+    try {
+      const response = await fetch(`${this.url}${this.planetEndpoint}`);
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      console.error(`Error ${error}`);
+      throw error;
+    }
+  }
+
+  async getShip() {
+    try {
+      const response = await fetch(`${this.url}${this.spaceShipEndpoint}`);
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      console.error(`Error ${error}`);
+      throw error;
+    }
+  }
+
+  async moveShip(id) {
+    try {
+      const fetchOptions = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+      };
+
+      const response = await fetch(
+        `${this.url}${this.spaceShipEndpoint}/${id}`,
+        fetchOptions
+      );
+      await response.json();
+    } catch (error) {
+      console.error(`Error ${error}`);
+      throw error;
+    }
+  }
+}
+
+const servicesInstance = new Services();
+export default servicesInstance;
