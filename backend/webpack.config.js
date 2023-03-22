4b4a63773cf21bd92d12d506f251d26a9e532f6d
SnakeMacbookPro <snakemacbookpro@SnakeMacbookPro-iMac-Pro.local>
2023-03-22 10:27:47 +0100

    webpacket added to project

---
+++ backend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/server.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+};
